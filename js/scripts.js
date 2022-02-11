1de3fd69e46e0c383525d5c451b25c15c600c814
Rian Carter <R.Carter86@hotmail.com>
2022-02-11 15:42:11 -0800

    change javascript method

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,34 @@
+
+
 var beep = "beep!"
 var boop = "boop!"
 var neighbor = "Won't you be my neighbor?"
+var test = "test"
 var mrRobo = function(input) {
   let numbers = [];
   for(let i = 0; i <= input; i++) {
     numbers.push(i);
   };
-  var numberStrings = [];
-  numbers.forEach(function(number) {
-    numberStrings.push(number.toString());
+
+  var numberStrings = numbers.map(function(number) {
+    return number.toString();
   });
 
-  numbers[numbers.indexOf("3")] = neighbor;
-  numbers[numbers.indexOf("2")] = boop;
-  numbers[numbers.indexOf("1")] = beep;
+  // numbers[numbers.indexOf("3")] = neighbor;
+  // numbers[numbers.indexOf("2")] = boop;
+  // numbers[numbers.indexOf("1")] = beep;
+
+  var mrRobo = numberStrings.map(function(numberString) {
+    if (numberString === "3") {
+      return numberString = neighbor;
+    } else if (numberString === "2") {
+      return numberString = boop;
+    } else if (numberString === "1") {
+      return numberString = beep;
+    } else {
+      return numberString;
+    }
+  });
   
-  return numbersStrings;
+  return mrRobo;
 };
