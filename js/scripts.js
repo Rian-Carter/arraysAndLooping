c5b87fdb241908ecc52be8ae1e26e157c6af9d39
Rian Carter <R.Carter86@hotmail.com>
2022-02-11 14:49:24 -0800

    complete objective for test1

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,10 @@ var beep = "beep!"
 var boop = "boop!"
 var neighbor = "Won't you be my neighbor?"
 var mrRobo = function(input) {
-  let str = "";
-  for(let i = 0; i < (input+1); i++) {
-    str = str + i;
-  }
-  var numbers = [str.split("")];
-  console.log(numbers);
+  let numStr = "";
+  for(let i = 0; i <= input; i++) {
+    numStr = numStr + i;
+  };
+  var numbers = [numStr.split("")];
+  return numbers;
 }
-
-$(document).ready(function() {
-  $("form#number").submit(function(event) {
-    event.preventDefault();
-    var input = parseInt($("input#input").val());
-    var result = mrRobo(input);
-    $("#results").text(result);
-  });
-});
