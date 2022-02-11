e0e2f6aacbf4914da1d90e19ab0111b20344514e
Rian Carter <R.Carter86@hotmail.com>
2022-02-11 15:06:56 -0800

    modify scripts.js to create array instead of string

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,10 @@ var beep = "beep!"
 var boop = "boop!"
 var neighbor = "Won't you be my neighbor?"
 var mrRobo = function(input) {
-  let numStr = "";
+  let numbers = [];
   for(let i = 0; i <= input; i++) {
-    numStr = numStr + i;
+    numbers.push(i);
   };
-  var numbers = numStr.split("");
   numbers[numbers.indexOf("1")] = beep;
   numbers[numbers.indexOf("2")] = boop;
   numbers[numbers.indexOf("3")] = neighbor;
