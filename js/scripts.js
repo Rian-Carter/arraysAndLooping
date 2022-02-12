aa754541499ca5f829adfc00570be0560a139237
Rian Carter <R.Carter86@hotmail.com>
2022-02-11 16:05:58 -0800

    reafactor code

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,6 @@
-
-
 var beep = "beep!"
 var boop = "boop!"
 var neighbor = "Won't you be my neighbor?"
-var test = "test"
 var mrRobo = function(input) {
   let numbers = [];
   for(let i = 0; i <= input; i++) {
@@ -14,16 +11,13 @@ var mrRobo = function(input) {
     return number.toString();
   });
 
-  // numbers[numbers.indexOf("3")] = neighbor;
-  // numbers[numbers.indexOf("2")] = boop;
-  // numbers[numbers.indexOf("1")] = beep;
 
   var mrRobo = numberStrings.map(function(numberString) {
-    if (numberString === "3") {
+    if (numberString.includes("3")) {
       return numberString = neighbor;
-    } else if (numberString === "2") {
+    } else if (numberString.includes("2")) {
       return numberString = boop;
-    } else if (numberString === "1") {
+    } else if (numberString.includes("1")) {
       return numberString = beep;
     } else {
       return numberString;
