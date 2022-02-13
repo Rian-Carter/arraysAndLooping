b9c6d789b1b9102c62181d72a32e3ce4ecb082c0
Rian Carter <R.Carter86@hotmail.com>
2022-02-12 17:09:43 -0800

    remove variables and use in function

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,29 @@
 var mrRobo = function(input) {
   let numbers = [];
   for(let i = 0; i <= input; i++) {
-    numbers.push(i);
+    numbers.push(i.toString());
   };
 
-  var numberStrings = numbers.map(function(number) {
-    return number.toString();
-  });
-
-  
-  var roboArray = numberStrings.map(function(numberString) {
-    if (numberString.includes("3")) {
-      return numberString = ("Won't you be my neighbor?");
-    } else if (numberString.includes("2")) {
-      return numberString = ("boop");
-    } else if (numberString.includes("1")) {
-      return numberString = ("beep");
+  var mrRobo = numbers.map(function(number) {
+    if (number.includes("3")) {
+      return number = ("Won't you be my neighbor?");
+    } else if (number.includes("2")) {
+      return number = ("boop!");
+    } else if (number.includes("1")) {
+      return number = ("beep!");
     } else {
-      return numberString;
-    }
-  });
-  
-  return roboArray;
+      return number;
+  };
+});
+  return mrRobo;
 };
+
+
+$(document).ready(function() {
+  $("form#inputtedNumber").submit(function(event) {
+    event.preventDefault();
+    var input = parseInt($("input#input").val());
+    var result = mrRobo(input);
+    $("#output").text(result);
+  });
+});
